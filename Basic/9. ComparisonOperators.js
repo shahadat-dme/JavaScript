d5ca14322e7efd6e31cc39a6bdec854615491091
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2020-03-17 00:13:21 +0600

    Add files via upload

---
+++ Basic/9. ComparisonOperators.js
@@ -0,0 +1,60 @@
+/** Comparison Operators & If... Else **/
+
+var isFCCGreat = true;
+
+if (isFCCGreat) {
+  console.log("Free Code Camp is amazing!");
+} else {
+  console.log("Free Code Camp is horrible!");
+};
+
+// 2nd part
+
+
+var isFCCGreat = false;
+
+if (isFCCGreat) {
+  console.log("Free Code Camp is amazing!");
+} else {
+  console.log("Free Code Camp is horrible!");
+};
+
+// Comparison Operators: > < <= >= == !=
+
+var age = 10;
+
+if (age >= 18) {
+  console.log("You are an adult!");
+} else if (age < 2) {
+  console.log("You are a baby!");
+} else if (age < 18) {
+  console.log("You are a child!");
+};
+
+// 3rd
+
+var age = 1;
+
+if (age >= 18) {
+  console.log("You are an adult!");
+} else if (age < 2) {
+  console.log("You are a baby!");
+} else if (age < 18) {
+  console.log("You are a child!");
+};
+
+//4rd
+var age = 27;
+
+if (age >= 18) {
+  console.log("You are an adult!");
+} else if (age < 2) {
+  console.log("You are a baby!");
+} else if (age < 18) {
+  console.log("You are a child!");
+};
+
+// Equil Sign
+if (age == 18) {console.log("You are eighteen.")};
+//Not Equil
+if (age != 18) {console.log("You are NOT eighteen.")};
