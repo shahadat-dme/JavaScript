c0a2f04ec4616b72cfce2a24aac1e4b9065d628f
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-02-23 00:52:52 +0600

    Add files via upload

---
+++ mjs/object.js
@@ -0,0 +1,11 @@
+var myObj ={
+    name: "JavaScript",
+    estd: "1995",
+    founder: "Brendan Eich",
+    ranking: 1,
+};
+var keys = Object.keys(myObj);
+var values = Object.values(myObj);
+
+console.log(keys);
+console.log(values);
