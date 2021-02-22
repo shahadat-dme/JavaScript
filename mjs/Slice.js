2866d39ddb42184bf9e67fb7be8e7c03c38ae66f
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-02-22 15:29:18 +0600

    Add files via upload

---
+++ mjs/Slice.js
@@ -0,0 +1,6 @@
+let num = [1,2,3,4,5];
+let res = num.slice(1,3);
+let r = num.slice(0,2);
+console.log(res);
+console.log(r);
+console.log(num);
---
+++ mjs/splice.js
@@ -0,0 +1,6 @@
+let num = [1,2,3,4,5,6,7];
+let res = num.splice(1,3,10,20,30);
+let r = num.splice(-1,2,10,20,30);
+console.log(res)
+console.log(num)
+console.log(r)
