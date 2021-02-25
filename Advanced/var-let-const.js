562e7ece447665b5dcc09ff3f355e382d957771c
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-02-25 12:53:31 +0600

    Add files via upload

---
+++ Advanced/var-let-const.js
@@ -0,0 +1,17 @@
+if(true){
+    var a ="var"
+    //var a = "As your wish you can use, same variable nam." 
+}
+console.log(a);
+
+
+if(true){
+    let b ="let"
+    b = "let1" //re sign kora jay.
+}
+console.log(b);
+
+if(true){
+    const b ="let"
+}
+console.log(b);
