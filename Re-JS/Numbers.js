4b55a8e91662521f06894ee6aacb8ce25ec1e0ce
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-03-07 23:34:42 +0600

    Add files via upload

---
+++ Re-JS/Numbers.js
@@ -0,0 +1,2 @@
+console.log(3 / 2);             // 1.5, not 1
+console.log(Math.floor(3 / 2)); // 1
---
+++ Re-JS/Strings.js
@@ -0,0 +1,3 @@
+console.log('hello, world'.replace('world', 'mars')); // "hello, mars"
+console.log('hello'.toUpperCase()); // "HELLO"
+console.log('hello'.charAt(0));
